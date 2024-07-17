afdefb9a5393856216d1b7c34d9e21d9e1740d03
AgBKartikey <kartikey.np2@gmail.com>
2024-07-17 18:27:52 +0530

    Update vendorsetup.sh
    No need to clone whole sepolicy.. Just Pick https://github.com/Nothing-phone-2-Development/android_device_qcom_sepolicy_vndr/commit/d9fa2f5f3c36dae723d9ca0899dc0ab85e149e9d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,40 +2,33 @@ echo 'Starting vendorsetup processes needed for your device'
 
 # Stuffs to rm -rf
 rm -rf hardware/qcom-caf/sm8450/display
-rm -rf device/qcom/sepolicy_vndr/sm8450
 rm -rf packages/apps/ParanoidGlyph
 
-echo 'Cloning Vendor tree [1/6]'
+echo 'Cloning Vendor tree [1/5]'
 # vendor/nothing/Pong
 git clone https://gitlab.com/AgBKartikey/vendor_nothing_Pong.git -b UDC vendor/nothing/Pong
 
 echo 'DONE'
 
-echo 'Cloning Kernel tree [2/6]'
+echo 'Cloning Kernel tree [2/5]'
 # kernel/nothing/sm8475
 git clone --depth=1 https://github.com/AgBKartikey/kernel_nothing_sm8475.git -b inline kernel/nothing/sm8475 && cd kernel/nothing/sm8475 && git submodule init && git submodule update --remote && cd ../../..
 
 echo 'DONE'
 
-echo 'Cloning Display Hal [3/6]'
+echo 'Cloning Display Hal [3/5]'
 # hardware/qcom-caf/sm8450/display
 git clone --depth=1 https://github.com/Nothing-phone-2-Development/android_hardware_qcom_display.git hardware/qcom-caf/sm8450/display
 
 echo 'DONE'
 
-echo 'Cloning sepolicy_vndr [4/6]'
-# device/qcom/sepolicy_vndr/sm8450
-git clone https://github.com/PongxViolet/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8450
-
-echo 'DONE'
-
-echo 'Cloning PAGlyph Repo [5/6]'
+echo 'Cloning PAGlyph Repo [4/5]'
 # packages/apps/ParanoidGlyph
 git clone https://github.com/ProjectGhostOS/android_packages_apps_ParanoidGlyph.git packages/apps/ParanoidGlyph
 
 echo 'DONE'
 
-echo 'Cloning Dolby [6/6]'
+echo 'Cloning Dolby [5/5]'
 # hardware/dolby
 git clone https://github.com/FlamingoOS-Devices/hardware_dolby.git hardware/dolby
 
